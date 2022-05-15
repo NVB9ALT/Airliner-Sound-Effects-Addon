694c69782872d7cc6b8f8ad2b03f0d1497e361d8
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-05-15 15:36:14 -0700

    Update Airliner SFX addon.js

--- Airliner SFX addon.js
+++ Airliner SFX addon.js
@@ -3,27 +3,40 @@ ui.notification.show("This addon is still in development. As such, running it ma
 
 //Modified by NVB9
 
+//variable to tell if the script has run or not
 var b737Sounds = new Boolean(0)
 
 function checkForBoeing() {
 
-if (b737Sounds == 0){
-if (geofs.aircraft.instance.id == 4 || geofs.aircraft.instance.id == 2769 || geofs.aircraft.instance.id == 2772 || geofs.aircraft.instance.id == 3011 || geofs.aircraft.instance.id == 3054) {
+if (geofs.aircraft.instance.id == 4 || geofs.aircraft.instance.id == 2769 || geofs.aircraft.instance.id == 2772 || geofs.aircraft.instance.id == 3011 || geofs.aircraft.instance.id == 3054) { //if the aircraft currently being flown is a 737
+if (b737Sounds == 0){ //if the script hasn't already run on this aircraft
 
+//running the script
 var script737 = document.createElement('script'); 
 script737.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFs-737-Immersion-SFX@main/index.js";
 document.body.appendChild(script737);
 script737.onload = function (){change737s()}
 
+//script has run now, so we change scriptHasRun to avoid having the script execute multiple times per aircraft instance
+//this avoids massive lag
 b737Sounds = 1
       }
+   }
+//if the aircraft isn't a 737
 else {
+//clearing the intervals when the aircraft isn't a 737 to avoid filling up the console with errors
 	clearInterval(soundInt)
 	clearInterval(tcasIntervalAnnounce)
+//making sure the script can run again next time a 737 is selected
 	b737Sounds = 0
-}
    }
 }
+
+//running the above function once per second
 let checkInterval = setInterval(function(){
 checkForBoeing()
 }, 1000)
+
+//Concorde engine sounds
+//Airbus alarms (except A220)
+//777 expansion
