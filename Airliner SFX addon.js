98c8eb6efe3393944c85b23604ae9270e2fd5485
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-08-14 14:33:36 -0700

    Update Airliner SFX addon.js

--- Airliner SFX addon.js
+++ Airliner SFX addon.js
@@ -11,7 +11,7 @@ if (b737Sounds == 0){ //if the script hasn't already run on this aircraft
 
 //running the script
 var script737 = document.createElement('script'); 
-script737.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFs-737-Immersion-SFX-Repo-V3@main/indexO.js";
+script737.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFs-737-Immersion-SFX-Repo-for-Realism-Addon@main/indexE.js";
 document.body.appendChild(script737);
 script737.onload = function (){change737s()}
 
@@ -22,13 +22,13 @@ b737Sounds = 1
    }
 //if the aircraft isn't a 737
 else {
-//clearing the intervals when the aircraft isn't a 737 to avoid filling up the console with errors
-   if (typeof soundInt != undefined) {
-	   clearInterval(soundInt)
-	}
-	if (typeof tcasIntervalAnnounce != undefined) {
-	   clearInterval(tcasIntervalAnnounce)
-	}
+//clearing the script when the aircraft isn't a 737 to avoid filling up the console with errors
+if (typeof soundInt != undefined) {
+   clearInterval(soundInt)
+	clearInterval(tcasIntervalAnnounce)
+} else {
+void(0)
+};
 //making sure the script can run again next time a 737 is selected
 	b737Sounds = 0
    }
@@ -53,22 +53,102 @@ script777.onload = function (){change777s()}
 
 b777sounds = 1
       }
-   }
-else {
+   } else {
 if (typeof effectInterval != undefined) {
    clearInterval(effectInterval)
+} else {
+   void(0)
 }
 	b777sounds = 0
    }
 }
 
-checkInterval = setInterval(function(){
+checkInterval1 = setInterval(function(){
 checkForBoeing777()
 }, 1000)
+//-----------------------------------------------------------------------------------------------------------------------------------------
+/*var a321sounds = new Boolean(0)
 
+function checkForAirbusA321() {
 
-//Future plans:
-/*
-Concorde engine sounds (maybe)
-Airbus alarms (except A220)
-*/
+if (geofs.aircraft.instance.id == 242) {
+if (a321sounds == 0){
+
+var scriptA321 = document.createElement('script'); 
+scriptA321.src="https://cdn.jsdelivr.net/gh/NVB9ALT/A321Neo-Plus-Addon@realism-addon/A321NeoF.js";
+document.body.appendChild(scriptA321);
+scriptA321.onload = function (){changeA321s()}
+
+a321sounds = 1
+      }
+   } else {
+if (typeof simInterval != undefined) {
+   clearInterval(simInterval)
+   clearInterval(alarmsInterval)
+} else {
+   void(0)
+}
+	a321sounds = 0
+   }
+}
+
+checkInterval2 = setInterval(function(){
+checkForAirbusA321()
+}, 1000)*/
+//-----------------------------------------------------------------------------------------------------------------------------------------
+/*Note: include stall and bank angle alarms*/
+//-----------------------------------------------------------------------------------------------------------------------------------------
+var airbusAlarms = new Boolean(0)
+//smart GPWS (excessive terrain closure rate trigger)
+function checkForAirbusAlarms() {
+if () { //all Airbuses except A321Neo and A220
+if (airbusAlarms == 0) {
+
+var scriptAA = document.createElement('script');
+scriptAA.src = "";
+document.body.appendChild(scriptAA);
+scriptAA.onload = function (){addAirbusAlarms()}
+
+airbusAlarms = 1
+   }
+} else {
+if (typeof alarmIntervalAirbus != undefined) {
+   clearInterval(alarmIntervalAirbus);
+} else {
+   void(0)
+}
+airbusAlarms = 0
+   }
+}
+
+checkIntervalAirbus = setInterval(function(){
+checkForAirbusAlarms()
+}, 1000)
+//-----------------------------------------------------------------------------------------------------------------------------------------
+var boeingAlarms = new Boolean(0)
+//smart GPWS (excessive terrain closure rate trigger)
+// https://https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/stall.ogg
+function checkForBoeingAlarms() {
+if () { //all Boeings except 737s and 777
+if (boeingAlarms == 0) {
+
+var scriptBA = document.createElement('script');
+scriptBA.src = "";
+document.body.appendChild(scriptBA);
+scriptBA.onload = function (){addBoeingAlarms()}
+
+boeingAlarms = 1
+   }
+} else {
+if (typeof alarmIntervalBoeing != undefined) {
+   clearInterval(alarmIntervalBoeing);
+} else {
+   void(0)
+}
+boeingAlarms = 0
+   }
+}
+
+checkIntervalBoeing = setInterval(function(){
+checkForBoeingAlarms()
+}, 1000)
